9391c6f96ed5adfe44aced97ef6b997a58ccbab4
Rajat <tyagirajat200@gmail.com>
2020-07-21 06:23:37 +0530

    first commit

--- server.js
+++ server.js
@@ -127,10 +127,10 @@ app.use('/api/user', UserRoutes)
 app.use('/api/chat', chatRoutes)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,"../",'dist/chat')))
+    app.use(express.static(path.join(__dirname,'dist/chat')))
 
     app.get('/*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../",'dist/chat','index.html'))
+        res.sendFile(path.join(__dirname,'dist/chat','index.html'))
     })
 }
 
