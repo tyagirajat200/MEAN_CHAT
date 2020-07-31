11eb50bb46dd816e39f55b4ff1d6f85f22b3c30d
Rajat <tyagirajat200@gmail.com>
2020-07-31 11:34:52 +0530

    first commit

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -4,4 +4,5 @@ module.exports = {
     IS_PROD: process.env.NODE_ENV === 'production',
     SESS_SECRET: process.env.SESS_SECRET,
     COOKIE_NAME: process.env.COOKIE_NAME,
+    PASSWORD : process.env.PASSWORD
 }
