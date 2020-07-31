1809f9b65c1d0287310f1ceabec823fe35d2b94d
Rajat <tyagirajat200@gmail.com>
2020-07-31 21:41:28 +0530

    first commit

--- src/app/video/outgoing-video/outgoing-video.component.ts
+++ src/app/video/outgoing-video/outgoing-video.component.ts
@@ -28,6 +28,21 @@ const iceServers = {
     { urls: 'stun:stun2.l.google.com:19302' },
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
+    {
+      urls: 'turn:192.158.29.39:3478?transport=udp',
+      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      username: '28224511:1379330808'
+    },
+    {
+      urls: 'turn:numb.viagenie.ca',
+      credential: 'muazkh',
+      username: 'webrtc@live.com'
+      },
+      {
+        urls: 'turn:numb.viagenie.ca',
+        credential: 'rajattyagi',
+        username: 'cu.16bcs1182@gmail.com'
+        },
   ],
 }
 
